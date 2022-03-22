819aa2475b7a4743b383241e0705c0ba50d6465f
Gregory Josephs <greg.josephs@gmail.com>
2022-03-22 10:25:44 -0400

    Added validation to deposit

--- src/routes/deposit.js
+++ src/routes/deposit.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { UserContext } from '../components/user-context';
 import Card from '../components/card';
 import { datedTransaction } from '../components/dated-transaction';
+import validate from '../components/validate';
 
 function Deposit() {
+    // Set the Context
     const ctx = React.useContext(UserContext);
+
+    // Set the state variables
     const [status, setStatus]     = React.useState('');
     const [deposit, setDeposit]   = React.useState(0);
-    const [message, setMessage]   = React.useState(null)
+    const [message, setMessage]   = React.useState(null);
+    const [enable, setEnable]     = React.useState(false);
     const [show, setShow]         = React.useState(() => {
         if (ctx.currentUser) {
             return true;
@@ -18,6 +23,11 @@ function Deposit() {
     
     function handleSubmit() {
         console.log(`Deposit is ${deposit}`);
+        // Display an error if the deposit is negative
+        if (deposit <= 0) {
+            setMessage('Please enter a number greater than zero');
+            return;
+        }
         let i = ctx.userIndex;
         let currentBalance = Number(ctx.users[i].balance);
         ctx.users[i].balance = currentBalance + Number(deposit);
@@ -31,6 +41,23 @@ function Deposit() {
 
     }
 
+    // Validate all fields using the validate function inported from validate
+    const validateThis = () => {
+        if (validate(deposit, 'please an amount to deposit', setStatus))
+            {
+                return true
+            } else {
+                return false
+            };
+    };
+
+    // A function to handle the onChange
+    const makeChange = (e, setThis) => {
+        setThis(e.currentTarget.value);
+        if (validateThis())
+            {setEnable(true)};
+    }; 
+
     return (
         <Card
             bgcolor="primary"
@@ -42,8 +69,8 @@ function Deposit() {
                     <h6>Your current balance is:</h6>
                     <h6>${ctx.currentUser.balance}</h6>
                     Deposit<br/>
-                    <input type="number" className="form-control" id="deposit" placeholder="Enter amount to deposit" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}/><br/>
-                    <button type="submit" className="btn btn-light" onClick={handleSubmit}>Deposit</button><br/><br/>
+                    <input type="number" className="form-control" id="deposit" placeholder="Enter amount to deposit" value={deposit} onChange={e => {makeChange(e, setDeposit)}}/><br/>
+                    <button type="submit" disabled={!enable} className="btn btn-light" onClick={handleSubmit}>Deposit</button><br/><br/>
                     {message && <h5>{message}</h5>}
 
 
