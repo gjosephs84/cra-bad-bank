a4e1fc7b4ec7a1dda12b4015f657a5ca2fe9b07a
Gregory Josephs <greg.josephs@gmail.com>
2022-03-22 10:46:34 -0400

    updated validate function

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,7 +1,6 @@
 const validate = (field, label, setStatus) => {
     if (!field) {
         setStatus('Error: ' + label);
-        // setTimeout(() => setStatus(''), 3000);
         return false;
     }
     setStatus(null);
