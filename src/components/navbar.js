b2884aa0cc7cf0f61af74fe2b1a50487be395150
Gregory Josephs <greg.josephs@gmail.com>
2022-04-01 08:24:01 -0400

    Added comments on conditionals

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
+
+// Context is being brought in here because it contains the state variables
+// required to conditionally show login/logout in the navbar
+// based on whether the login has actually occurred or not
+
 import { UserContext } from './user-context';
 
 function NavigationBar() {
+    // Setting the context as ctx
     const ctx = React.useContext(UserContext);
+    // Grabbing the state variable from context
     const [loggedIn, setLoggedIn] = ctx.loginState;
     return (
         <Navbar className="color-nav" variant="light" expand="lg">
@@ -19,6 +26,7 @@ function NavigationBar() {
                         <LinkContainer to="/createaccount">
                             <Nav.Link title="Create a New Account">Create Account</Nav.Link>
                         </LinkContainer>
+{/* Conditionally rendering the login option if loggedIn is false */}
                         {!loggedIn && <LinkContainer to="login">
                             <Nav.Link title="Log In">Login</Nav.Link>
                         </LinkContainer>}
@@ -34,6 +42,7 @@ function NavigationBar() {
                         <LinkContainer to="alldata">
                             <Nav.Link title="See all User Data">All Data</Nav.Link>
                         </LinkContainer>
+{/* Conditionally rendering the logout option if loggedIn is true */}
                         {loggedIn && <LinkContainer to="login">
                             <Nav.Link title="Log Out">Logout</Nav.Link>
                         </LinkContainer>}
